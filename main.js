531d7a4ee214b40047f95ca507fc4b3d48c7d094
pogianeli <130576465+pogianeli@users.noreply.github.com>
2024-06-10 11:11:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
@@ -76,7 +75,6 @@ function classificaSenha(tamanhoAlfabeto){
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
-
 
 
 
